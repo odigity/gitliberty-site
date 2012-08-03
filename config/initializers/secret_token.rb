61868ad538d202cdcf2e964880744a3574a5d9ad
Ofer Nave <odigity@gmail.com>
2012-08-02 23:16:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitliberty::Application.config.secret_token = '8da87098f952aef19551c5f007340d48999130020942077b650467e3e63851c35b64e7345f650f04b55848c84ab56ba930952b5a43adf00e44e36303664e826b'
