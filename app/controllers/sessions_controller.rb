ac3c716ca945b5e9042f192a30b6fc72688e1911
Ofer Nave <odigity@gmail.com>
2012-08-03 12:47:07 -0400

    added debug statements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
+    puts "***** SessionsController.create"
+    puts "***** auth: #{env['omniauth.auth']}"
     user = User.from_omniauth(env['omniauth.auth'])
+    puts "***** user: #{user}"
     session[:user_id] = user.id
     redirect_to root_url, notice: 'You have successfully logged in!'
   end
@@ -12,6 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def failure
+    puts "***** SessionsController.failure"
+    puts "***** auth: #{env['omniauth.auth']}"
   end
 
 end
